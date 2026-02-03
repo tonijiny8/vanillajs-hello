7aa57b721ec2b53aea53f59cdefe4e3d04d01f59
tonijiny8 <tonijinyang@gmail.com>
2026-02-03 22:09:51 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+    
+    let p1 = Math.floor (Math.random()*who.length);
+    let p2 = Math.floor (Math.random()*action.length);
+    let p3 = Math.floor (Math.random()*what.length);
+    let p4 = Math.floor (Math.random()*when.length);+ 
+
+    document.querySelector("#excuse").innerHTM= who[p1] + action[p2] + what[p3] + when[p4];
+
+
+}
